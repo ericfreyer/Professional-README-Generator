461ad531efc8e43e8299f1f3ea5750a0781632f4
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-01-17 18:11:53 -0500

    created notes for what to add/code layout

--- develop/index.js
+++ develop/index.js
@@ -2,3 +2,26 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
+
+
+//array of questions to ask
+
+
+
+
+//write function for prompts
+
+
+
+
+
+
+//generate readme
+
+
+
+//generate markdown
+
+
+
+//writefile
