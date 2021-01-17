0f0a385d82ec53ae44007cebdf758465d3035931
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-01-17 18:48:22 -0500

    got user questions to properly log

--- develop/index.js
+++ develop/index.js
@@ -3,10 +3,52 @@ const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
+userQuestions()
 //array of questions to ask
+function userQuestions() {
 
-
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your Github username?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project's title?"
+        },  
+        {
+            type: "input",
+            name: "install",
+            message: "Explain the installation process."
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a brief description of your project."
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "Were there any special licenses used for this project?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please explain the usage of the program."
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Are you running any tests for this program?"
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Are there any contribution rules?"
+        },
+    ])}
 
 
 //write function for prompts
@@ -24,4 +66,4 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 
 
-//writefile
+//write file 
